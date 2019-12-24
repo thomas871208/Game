ac3f14c71fd91c3cfd4174aa71309322563866f1
miia <miia@com.tw>
2019-12-24 16:48:59 +0800

    wwwwwwww ahh ahh~~~

--- app/src/main/java/com/tts/game/MainActivity.java
+++ app/src/main/java/com/tts/game/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    SpaceView game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.arrow_down).setOnClickListener(this);
         findViewById(R.id.arrow_left).setOnClickListener(this);
         findViewById(R.id.arrow_right).setOnClickListener(this);
+        game = findViewById(R.id.game);
+
+
+
+    }
+    public void MoveUp(){
+        game.setPoxsY(game.getPoxsY() - 50);
+    }
+    public  void MoveDown(){
+        game.setPoxsY(game.getPoxsY() + 50);
+    }
+    public  void MoveLeft(){
+        game.setPoxsX(game.getPoxsX() - 50);
+    }
+    public  void MoveRight(){
+        game.setPoxsX(game.getPoxsX() + 50);
     }
 
     @Override
@@ -23,15 +40,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.arrow_up:
                 Log.d("mainactivity", "onClick: up");
+                MoveUp();
+                game.invalidate();
                 break;
             case R.id.arrow_down:
                 Log.d("mainactivity", "onClick:down ");
+                MoveDown();
+                game.invalidate();
                 break;
             case R.id.arrow_left:
                 Log.d("mainactivity", "onClick: left");
+                MoveLeft();
+                game.invalidate();
                 break;
             case R.id.arrow_right:
                 Log.d("mainactivity", "onClick: right");
+                MoveRight();
+                game.invalidate();
                 break;
         }
 
---
+++ app/src/main/java/com/tts/game/SpaceView.java
@@ -0,0 +1,55 @@
+package com.tts.game;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import javax.security.auth.PrivateCredentialPermission;
+
+import androidx.annotation.Nullable;
+
+public class SpaceView extends View {
+    private float poxsX , poxsY = 400;
+
+
+
+    public SpaceView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        canvas.drawLine(400,0,0,600, paint);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.neko);
+        canvas.drawBitmap(bitmap,poxsX,poxsY,paint);
+        Log.d("game", "onDraw: "+poxsX +"+"+poxsY);
+    }
+
+    public float getPoxsX() {
+
+        return poxsX;
+    }
+
+    public float getPoxsY() {
+        return poxsY;
+    }
+
+    public void setPoxsX(float poxsX) {
+        if (poxsX >= 0 && poxsX <= getHeight()) {
+            this.poxsX = poxsX;
+        }
+    }
+
+    public void setPoxsY(float poxsY) {
+        if(poxsY >= 0 && poxsY<= getWidth()){
+            this.poxsY = poxsY;
+        }
+    }
+}
